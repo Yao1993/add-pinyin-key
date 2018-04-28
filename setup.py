2b60156df7078a6adc53076ffcec831c5bb766b5
yaoxu <yaoxu@mail.ustc.edu.cn>
2018-04-28 13:45:20 +0800

    Add long descroption

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+import os
+
+def long_description():
+    with open(os.path.join(".", 'README.md'),
+              encoding='utf8') as fp:
+        return fp.read()
 
 setup(
     name='add-pinyin-key',
-    version='0.0.1',
+    version='0.0.2',
     description='Add pinyin keys to Chinese bib entries',
     license='MIT',
     packages=find_packages(),
@@ -18,6 +24,7 @@ setup(
             'add_pinyin_key = add_pinyin_key.__main__:main',
         ],
     },
+    long_description=long_description(),
     long_description_content_type='text/markdown',
     url='https://github.com/Yao1993/add-pinyin-key'
 )
