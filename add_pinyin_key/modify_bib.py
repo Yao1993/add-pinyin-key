3963f28a8552d75665bfaa122655a0c6ae895d1b
yaoxu <yaoxu@mail.ustc.edu.cn>
2018-04-28 13:42:00 +0800

    Add --common-strings option

--- add_pinyin_key/modify_bib.py
+++ add_pinyin_key/modify_bib.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import bibtexparser
+from bibtexparser.bparser import BibTexParser
 import re
 from pypinyin import pinyin, Style
 
@@ -16,14 +17,20 @@ def if_containing_chinese(test_string):
 
 def add_pinyin_key_to_bib_database(db):
     for entry in db.entries:
-        if if_containing_chinese(entry["author"]):
-            entry["key"] = to_pinyin(entry["author"])
+        try:
+            if if_containing_chinese(entry["author"]):
+                entry["key"] = to_pinyin(entry["author"])
+        except KeyError:
+            print("Entry dose not contain author info. Skip.")
+            print(entry)
     return db
 
 
-def add_pinyin_key_to_bib_file(input_bib, output_bib):
+def add_pinyin_key_to_bib_file(input_bib, output_bib, using_common_strings):
     with open(input_bib, encoding="utf-8") as input_file:
-        bib_database = bibtexparser.load(input_file)
+        bibtex_str = input_file.read()
+    parser = BibTexParser(common_strings=using_common_strings, ignore_nonstandard_types=False)
+    bib_database = bibtexparser.loads(bibtex_str, parser)
 
     writer = bibtexparser.bwriter.BibTexWriter()
     with open(output_bib, encoding="utf-8", mode="w") as output_file:
--- add_pinyin_key/runner.py
+++ add_pinyin_key/runner.py
@@ -9,9 +9,14 @@ def main():
     parser = argparse.ArgumentParser(description='Add pinyin keys to chinese bib entries.')
     parser.add_argument('input_bib')
     parser.add_argument('output_bib')
+
+    parser.add_argument('--common-strings', dest='using_common_strings', action='store_true', help='If your bibtex contains months defined as strings such as month = jan, you will need this option.')
+    # parser.add_argument('--no-common-strings', dest='using_common_strings', action='store_false', help='default')
+    parser.set_defaults(feature=False)
+
     args = parser.parse_args()
 
-    add_pinyin_key_to_bib_file(args.input_bib, args.output_bib)
+    add_pinyin_key_to_bib_file(args.input_bib, args.output_bib, args.using_common_strings)
 
 
 if __name__ == "__main__":
